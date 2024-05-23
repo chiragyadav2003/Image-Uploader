de0ff41958d4d15b334964ab49e49c97c3f17a71
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-23 18:52:32 +0530

    bug fix - auth route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,40 +1,42 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
 import User from "../models/User.js";
-import { JWT_SECRET } from '../dotenv.config';
+import { JWT_SECRET } from '../dotenv.config.js';
 
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
-    const { username, password } = req.body;
+    const { email, password } = req.body;
     try {
         const userExists = await User.findOne({
-            username
+            email
         });
         if (userExists) {
             return res.status(400).json({
                 message: 'User already exists'
             });
         }
-        const user = new User({ username, password });
+        const user = new User({ email, password });
+
         await user.save();
-        res.status(201).json({
-            message: 'User created'
+
+        return res.status(201).json({
+            message: 'User created successfully'
         });
     } catch (error) {
         console.log(error?.message);
-        res.status(500).json({
-            message: 'Server error'
+        return res.status(500).json({
+            error: error?.message,
+            message: 'signup error'
         });
     }
 });
 
 router.post('/login', async (req, res) => {
-    const { username, password } = req.body;
+    const { email, password } = req.body;
     try {
         const user = await User.findOne({
-            username
+            email
         });
 
         if (!user || !(await user.matchPassword(password))) {
@@ -49,21 +51,21 @@ router.post('/login', async (req, res) => {
             expiresIn: '1d'
         });
 
-        res.cookie('Authorization', `Bearer ${token}`);
+        res.cookie('token', token);
         res.status(200).json({
             message: 'User logged in'
         });
     } catch (error) {
         console.log(error?.message);
-        res.status(500).json({
-            message: 'Server error',
-            error: error?.message
+        res.status(400).json({
+            error: error?.message,
+            message: 'login error',
         });
     }
 });
 
 router.post('/logout', (req, res) => {
-    res.clearCookie('Authorization');
+    res.clearCookie('token');
     res.status(200).json({
         message: 'User logged out'
     });
