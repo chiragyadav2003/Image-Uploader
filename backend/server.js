e5eaf73bbdea03e54fbe6c40b82fd44ca31117a7
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-23 18:53:22 +0530

    update server file

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,19 @@ import cookieParser from 'cookie-parser';
 
 import connectDb from "./db.js";
 import { PORT as dbPORT } from "./dotenv.config.js"
+import authRoutes from "./routes/auth.js"
+import folderRoutes from "./routes/folder.js";
+import imageRoutes from "./routes/image.js";
 
 const app = express();
-
 connectDb();
 
 app.use(cookieParser());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/folders", folderRoutes);
+app.use("/api/images", imageRoutes);
 
 const PORT = dbPORT || 5000;
 app.listen(PORT, () => {
