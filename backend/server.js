5d960a6cc27b7a65c9a89f2f3593f118f60d328a
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-05-24 22:05:46 +0530

    adding cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import connectDb from "./db.js";
 import { PORT as dbPORT } from "./dotenv.config.js"
@@ -7,8 +8,15 @@ import authRoutes from "./routes/auth.js"
 import folderRoutes from "./routes/folder.js";
 import imageRoutes from "./routes/image.js";
 
+
 const app = express();
 connectDb();
+
+// Configure CORS middleware
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from localhost:5173
+    credentials: true // Allow sending of cookies
+}));
 
 app.use(cookieParser());
 app.use(express.json());
